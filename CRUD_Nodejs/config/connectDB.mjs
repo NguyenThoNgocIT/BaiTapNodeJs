7a011fbe155910b1f09feae40b0ec67e0eb0b466
NguyenThoNgocIT <nguyenthongoc22072004@gmail.com>
2024-11-16 09:14:18 +0700

    bai thi booking

--- CRUD_Nodejs/config/connectDB.mjs
+++
@@ -1,9 +0,0 @@
-// Kết nối MongoDB
-import mongoose from "mongoose";
-export const connectMongooseDB = async (uri)=>{
-  try {
-        mongoose.connect(uri)
-  } catch (error) {
-    console.log("Error 404 no connect DB")
-  }
-}
